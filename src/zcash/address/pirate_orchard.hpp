8d75b7697d449afb36f7fe5c5fbbc112883021f5
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2024-12-28 01:55:49 +0200

    Update pirate_orchard.hpp
    ## 1. Added Utility Function for Secure Memory Cleansing
    - **Change**: Introduced `secure_cleanse` to standardize and simplify memory cleansing.
    
    ## 2. Improved Documentaion
    - **Change**: Added and improved on existing documentation of key methods

--- src/zcash/address/pirate_orchard.hpp
+++ src/zcash/address/pirate_orchard.hpp
@@ -13,6 +13,7 @@
 namespace libzcash
 {
 
+// Define serialized sizes for Orchard keys and addresses.
 const size_t SerializedOrchardPaymentAddressSize = 43;
 const size_t SerializedOrchardOutgoingKeySize = 32;
 const size_t SerializedOrchardIncomingViewingKeySize = 64;
@@ -21,6 +22,7 @@ const size_t SerializedOrchardExtendedFullViewingKeySize = 137;
 const size_t SerializedOrchardSpendingKeySize = 32;
 const size_t SerializedOrchardExtendedSpendingKeySize = 73;
 
+// Define data types for Orchard keys and addresses.
 typedef std::array<unsigned char, SerializedOrchardPaymentAddressSize> OrchardPaymentAddress_t;
 typedef std::array<unsigned char, SerializedOrchardIncomingViewingKeySize> OrchardIncomingViewingKey_t;
 typedef std::array<unsigned char, SerializedOrchardFullViewingKeySize> OrchardFullViewingKey_t;
@@ -28,14 +30,23 @@ typedef std::array<unsigned char, SerializedOrchardSpendingKeySize> OrchardSpend
 typedef std::array<unsigned char, SerializedOrchardExtendedSpendingKeySize> OrchardExtendedSpendingKey_t;
 typedef std::array<unsigned char, ZC_DIVERSIFIER_SIZE> diversifier_t;
 
-//! Orchard functions.
+// Add secure memory cleansing utility.
+template <typename T>
+void secure_cleanse(T& obj) {
+    memory_cleanse(reinterpret_cast<void*>(obj.data()), obj.size());
+}
+
+//! Orchard functions and classes.
 class OrchardPaymentAddressPirate
 {
 public:
     diversifier_t d;
     uint256 pk_d;
 
-    OrchardPaymentAddressPirate() : d(), pk_d() {}
+    OrchardPaymentAddressPirate() : d(), pk_d() {
+        secure_cleanse(d);
+        secure_cleanse(pk_d);
+    }
     OrchardPaymentAddressPirate(diversifier_t d, uint256 pk_d) : d(d), pk_d(pk_d) {}
 
     ADD_SERIALIZE_METHODS;
@@ -50,7 +61,7 @@ public:
     //! Get the 256-bit SHA256d hash of this payment address.
     uint256 GetHash() const;
 
-    // Get serialized bytes of an Orchard Address
+    //! Get serialized bytes of an Orchard Address.
     OrchardPaymentAddress_t ToBytes() const;
 
     friend inline bool operator==(const OrchardPaymentAddressPirate& a, const OrchardPaymentAddressPirate& b)
@@ -69,7 +80,9 @@ class OrchardOutgoingViewingKey
 public:
     uint256 ovk;
 
-    OrchardOutgoingViewingKey() : ovk() {}
+    OrchardOutgoingViewingKey() : ovk() {
+        secure_cleanse(ovk);
+    }
     OrchardOutgoingViewingKey(uint256 ovk) : ovk(ovk) {}
 
     ADD_SERIALIZE_METHODS;
@@ -96,10 +109,13 @@ public:
     uint256 dk;
     uint256 ivk;
 
-    OrchardIncomingViewingKeyPirate() : dk(), ivk() {}
+    OrchardIncomingViewingKeyPirate() : dk(), ivk() {
+        secure_cleanse(dk);
+        secure_cleanse(ivk);
+    }
     OrchardIncomingViewingKeyPirate(uint256 dk, uint256 ivk) : dk(dk), ivk(ivk) {}
 
-    // Can pass in diversifier for Sapling addr
+    // Returns the payment address for a given diversifier.
     std::optional<OrchardPaymentAddressPirate> address(diversifier_t d) const;
 
     ADD_SERIALIZE_METHODS;
@@ -129,8 +145,13 @@ public:
     uint256 rivk;
     bool internal; // key scope
 
-    OrchardFullViewingKeyPirate() : ak(), nk(), rivk() {}
-    OrchardFullViewingKeyPirate(uint256 ak, uint256 nk, uint256 rivk) : ak(ak), nk(nk), rivk(rivk) {}
+    OrchardFullViewingKeyPirate() : ak(), nk(), rivk() {
+        secure_cleanse(ak);
+        secure_cleanse(nk);
+        secure_cleanse(rivk);
+    }
+    OrchardFullViewingKeyPirate(uint256 ak, uint256 nk, uint256 rivk)
+        : ak(ak), nk(nk), rivk(rivk) {}
 
     ADD_SERIALIZE_METHODS;
 
@@ -142,23 +163,31 @@ public:
         READWRITE(rivk);
     }
 
-    // Get the finger print of the Fullviewing key
+    //! Get the fingerprint of the Full Viewing Key.
     uint256 GetFingerprint() const;
 
-    // Get the outgoing viewing keys associated to a full viewing key
+    //! Retrieve the outgoing viewing key associated with this full viewing key.
     std::optional<OrchardOutgoingViewingKey> GetOVK() const;
+
+    //! Retrieve the outgoing viewing key for internal purposes.
     std::optional<OrchardOutgoingViewingKey> GetOVKinternal() const;
 
-    // Get the incoming viewing keys associated to a full viewing key
+    //! Retrieve the incoming viewing key associated with this full viewing key.
     std::optional<OrchardIncomingViewingKeyPirate> GetIVK() const;
+
+    //! Retrieve the incoming viewing key for internal purposes.
     std::optional<OrchardIncomingViewingKeyPirate> GetIVKinternal() const;
 
-    // Get the default addresses associated to a full viewing key
+    //! Retrieve the default payment address associated with this full viewing key.
     std::optional<OrchardPaymentAddressPirate> GetDefaultAddress() const;
+
+    //! Retrieve the default payment address for internal purposes.
     std::optional<OrchardPaymentAddressPirate> GetDefaultAddressInternal() const;
 
-    // Get the addresses of a given diversifier associated to a full viewing key
+    //! Retrieve the payment address for a given diversifier associated with this full viewing key.
     std::optional<OrchardPaymentAddressPirate> GetAddress(blob88 diversifier) const;
+
+    //! Retrieve the payment address for internal purposes for a given diversifier.
     std::optional<OrchardPaymentAddressPirate> GetAddressInternal(blob88 diversifier) const;
 
     friend inline bool operator==(const OrchardFullViewingKeyPirate& a, const OrchardFullViewingKeyPirate& b)
@@ -173,13 +202,14 @@ public:
     }
 };
 
-
 class OrchardSpendingKeyPirate
 {
 public:
     uint256 sk;
 
-    OrchardSpendingKeyPirate() : sk() {}
+    OrchardSpendingKeyPirate() : sk() {
+        secure_cleanse(sk);
+    }
     OrchardSpendingKeyPirate(uint256 sk) : sk(sk) {}
 
     ADD_SERIALIZE_METHODS;
@@ -190,17 +220,19 @@ public:
         READWRITE(sk);
     }
 
-    // Generate a random spendingkey
+    //! Generate a random spending key.
     std::optional<OrchardSpendingKeyPirate> random();
 
-    // Check sk byte validity
+    //! Validate the spending key bytes.
     bool IsValid();
 
-    // Get Full Viewing Key
+    //! Retrieve the Full Viewing Key associated with this spending key.
     std::optional<OrchardFullViewingKeyPirate> GetFVK() const;
 
-    // Get Default addresses assoicated to a spending key
+    //! Retrieve the default payment address associated with this spending key.
     std::optional<OrchardPaymentAddressPirate> GetDefaultAddress() const;
+
+    //! Retrieve the default payment address for internal purposes.
     std::optional<OrchardPaymentAddressPirate> GetDefaultAddressInternal() const;
 
     friend inline bool operator==(const OrchardSpendingKeyPirate& a, const OrchardSpendingKeyPirate& b)
